e59d4a4357925898c6ac6790979a153d41bb025c
Murat <muratnysambaev93@gmail.com>
2020-03-31 13:32:13 +0600

    Extracted app bootstrap

--- api/public/index.php
+++ api/public/index.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 use Slim\Factory\AppFactory;
 use Psr\Container\ContainerInterface;
+use Slim\App;
 
 http_response_code(500);
 
@@ -12,10 +13,7 @@ require __DIR__ . '/../vendor/autoload.php';
 /** @var ContainerInterface $container */
 $container = require __DIR__ . '/../config/container.php';
 
-$app = AppFactory::createFromContainer($container);
-
-(require __DIR__ . '/../config/middleware.php')($app, $container);
-
-(require __DIR__ . '/../config/routes.php')($app);
+/** @var App $app */
+$app = (require __DIR__ . '/../config/app.php')($container);
 
 $app->run();
