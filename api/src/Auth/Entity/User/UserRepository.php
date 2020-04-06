b9bfe2146d4c6d11423d4d5ce7b04333dd4ad717
Murat <muratnysambaev93@gmail.com>
2020-04-06 14:04:36 +0600

    Added user remove use case

--- api/src/Auth/Entity/User/UserRepository.php
+++ api/src/Auth/Entity/User/UserRepository.php
@@ -13,6 +13,7 @@ interface UserRepository
     public function findByJoinConfirmToken(string $token): ?User;
     public function findByPasswordResetToken(string $token): ?User;
     public function findByNewEmailToken(string $token): ?User;
+    public function remove(User $user): void;
     /**
      * @param Id $id
      * @return User
