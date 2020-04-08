9513fc3c22ab5bbd6cd11df272dbff43355ea622
Murat <muratnysambaev93@gmail.com>
2020-04-08 12:11:15 +0600

    Generated migration

--- api/config/dependencies.php
+++ api/config/dependencies.php
@@ -1,9 +1,10 @@
 <?php
 
+declare(strict_types=1);
+
 use Laminas\ConfigAggregator\ConfigAggregator;
 use Laminas\ConfigAggregator\PhpFileProvider;
 
-declare(strict_types=1);
 
 $aggregator = new ConfigAggregator([
     new PhpFileProvider(__DIR__ . '/common/*.php'),
