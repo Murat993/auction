5a96b000c963ac4d62265f9b11c1631d4ef4a486
Murat <muratnysambaev93@gmail.com>
2020-04-10 17:08:18 +0600

    Added Sentry error capturing

--- api/config/common/errors.php
+++ api/config/common/errors.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 use App\ErrorHandler\LogErrorHandler;
 use Psr\Container\ContainerInterface;
+use App\ErrorHandler\SentryDecorator;
 use Psr\Http\Message\ResponseFactoryInterface;
 use Psr\Log\LoggerInterface;
 use Slim\Interfaces\CallableResolverInterface;
@@ -33,7 +34,9 @@ return [
         $logger = $container->get(LoggerInterface::class);
 
         $middleware->setDefaultErrorHandler(
-            new LogErrorHandler($callableResolver, $responseFactory, $logger)
+            new SentryDecorator(
+                new LogErrorHandler($callableResolver, $responseFactory, $logger)
+            )
         );
 
         return $middleware;
