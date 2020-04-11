22eb1431f35a3a95415f3170c28583cbc34d18ce
Murat <muratnysambaev93@gmail.com>
2020-04-11 13:20:32 +0600

    Added tests fixtures

--- api/tests/Functional/WebTestCase.php
+++ api/tests/Functional/WebTestCase.php
@@ -9,6 +9,11 @@ use Psr\Container\ContainerInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use Slim\App;
 use Slim\Psr7\Factory\ServerRequestFactory;
+use Doctrine\Common\DataFixtures\AbstractFixture;
+use Doctrine\Common\DataFixtures\Executor\ORMExecutor;
+use Doctrine\Common\DataFixtures\Loader;
+use Doctrine\Common\DataFixtures\Purger\ORMPurger;
+use Doctrine\ORM\EntityManagerInterface;
 
 
 class WebTestCase extends TestCase
@@ -26,6 +31,26 @@ class WebTestCase extends TestCase
     {
         return (new ServerRequestFactory())->createServerRequest($method, $path);
     }
+
+    /**
+     * @param array<string|int,string> $fixtures
+     */
+    protected function loadFixtures(array $fixtures): void
+    {
+        /** @var ContainerInterface $container */
+        $container = $this->app()->getContainer();
+        $loader = new Loader();
+        foreach ($fixtures as $name => $class) {
+            /** @var AbstractFixture $fixture */
+            $fixture = $container->get($class);
+            $loader->addFixture($fixture);
+        }
+        /** @var EntityManagerInterface $em */
+        $em = $container->get(EntityManagerInterface::class);
+        $executor = new ORMExecutor($em, new ORMPurger($em));
+        $executor->execute($loader->getFixtures());
+    }
+
 
     protected function app(): App
     {
